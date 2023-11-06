d239617d81151bd671ca6bee808195ad7976c229
Franz Schnyder <schnyderfranz@gmail.com>
2023-11-06 19:27:34 +0100

    Dummy values

--- secrets.defaults.cmake
+++ secrets.defaults.cmake
@@ -1,1 +1,9 @@
 message(WARNING "load 'default_secrets.cmake' file => create a copy and rename to secrets.cmake!")
+
+set(WIFI_SSID myWifi)
+set(WIFI_PASSWORD myWifiPwd)
+
+set(MQTT_SERVER test.mosquitto.org)
+set(MQTT_CLIENT_ID pico)
+# set(MQTT_USER myMqttUser)
+# set(MQTT_PASSWORD myMqttUserPwd)
